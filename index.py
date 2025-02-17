baf243ad86b1c0a3c1a1b878ad38e6d234d0bd10
skylee01244 <skylee01244@gmail.com>
2025-02-18 03:19:43 +1100

    added database

---
+++ index.py
@@ -0,0 +1,32 @@
+from PyQt6.QtCore import * 
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import * 
+import sys
+
+from PyQt6.uic import loadUiType
+
+ui,_ = loadUiType('library.ui')
+
+class MainApp(QMainWindow, ui):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+    
+    def Handle_UI_Changes(self):
+        pass
+
+    def Handle_Buttons(self):
+        pass
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
+
